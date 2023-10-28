c6c962f7d1f8f65ca9ae36514ce6083c80a20e1a
Yeremmi789 <Yeremmi789>
2023-10-28 11:06:11 -0600

    creacion de la calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'practicaDev';
+  
+  form:FormGroup = new FormGroup({});
+
+  variable1 = 0;
+  variable2 = 0;
+  resultado = 0;
+
+
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+  }
+
+  ngOnInit(): void {}
+
+  crearFormulario() {
+    this.form = this.fb.group({
+      variable1: [null, [Validators.required, Validators.min(0)]],
+      variable2: [null, [Validators.required, Validators.min(0)]],
+    });
+  }
+  
+
+  operacionRealizada: string = '';
+
+realizarOperacion(operacion: string) {
+  if (this.form.valid) {
+    this.variable1 = this.form.value.variable1;
+    this.variable2 = this.form.value.variable2;
+
+    switch (operacion) {
+      case 'SUMAR':
+        this.resultado = this.variable1 + this.variable2;
+        this.operacionRealizada = 'SUMAR';
+        break;
+      case 'RESTAR':
+        this.resultado = this.variable1 - this.variable2;
+        this.operacionRealizada = 'RESTAR';
+        break;
+      case 'MULTIPLICAR':
+        this.resultado = this.variable1 * this.variable2;
+        this.operacionRealizada = 'MULTIPLICAR';
+        break;
+      case 'DIVIDIR':
+        if (this.variable2 === 0) {
+          this.resultado = 0;
+        } else {
+          this.resultado = this.variable1 / this.variable2;
+        }
+        this.operacionRealizada = 'DIVIDIR';
+        break;
+      default:
+        break;
+    }
+  }
+}
+
 }
